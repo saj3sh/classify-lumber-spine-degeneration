2d765f4a0a0422f00cf10e82ca56a396adaf680b
Sajesh Adhikari <saj3sh@Sajeshs-MacBook-Air.local>
2024-11-08 12:41:15 -0600

    added steps for preprocessing

---
+++ src/pre_process.py
@@ -0,0 +1,45 @@
+# read data
+import os
+import pandas as pd
+
+
+train_path = "/Users/saj3sh/Downloads/rsna-2024-lumbar-spine-classification/"
+
+train  = pd.read_csv(train_path + 'train.csv')
+label = pd.read_csv(train_path + 'train_label_coordinates.csv')
+train_desc  = pd.read_csv(train_path + 'train_series_descriptions.csv')
+test_desc   = pd.read_csv(train_path + 'test_series_descriptions.csv')
+sub         = pd.read_csv(train_path + 'sample_submission.csv')
+
+# Function to generate image paths based on directory structure
+def generate_image_paths(df, data_dir):
+    image_paths = []
+    for study_id, series_id in zip(df['study_id'], df['series_id']):
+        study_dir = os.path.join(data_dir, str(study_id))
+        series_dir = os.path.join(study_dir, str(series_id))
+        images = os.listdir(series_dir)
+        image_paths.extend([os.path.join(series_dir, img) for img in images])
+    return image_paths
+
+# Generate image paths for train and test data
+train_image_paths = generate_image_paths(train_desc, f'{train_path}/train_images')
+test_image_paths = generate_image_paths(test_desc, f'{train_path}/test_images')
+
+print(train_image_paths[2])
+
+import pydicom
+import matplotlib.pyplot as plt
+
+# Function to open and display DICOM images
+def display_dicom_images(image_paths):
+    plt.figure(figsize=(15, 5))  # Adjust figure size if needed
+    for i, path in enumerate(image_paths[:3]):
+        ds = pydicom.dcmread(path)
+        plt.subplot(1, 3, i+1)
+        plt.imshow(ds.pixel_array, cmap=plt.cm.bone)
+        plt.title(f"Image {i+1}")
+        plt.axis('off')
+    plt.show()
+
+# Display the first three DICOM images
+display_dicom_images(train_image_paths)
